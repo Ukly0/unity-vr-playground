a5171e8d713ae3539b13b44b94e58e8c93b3c05c
Ukly0 <aeg.expositogarcia@gmail.com>
2026-02-10 14:31:20 +0100

    Teleport works

--- VR Room Project/Assets/Scripts/AnalogClockController.cs
+++ VR Room Project/Assets/Scripts/AnalogClockController.cs
@@ -6,26 +6,34 @@ public class AnalogClockController : MonoBehaviour
     [SerializeField] private Transform hours, minutes, seconds;
     private Quaternion initialHours, initialMinutes, initialSeconds;
 
+    float hourAngle = (DateTime.Now.Hour % 12) * 30f + DateTime.Now.Minute * 0.5f;
+    float minutesAngle = DateTime.Now.Minute * 6f;
+    float secondsAngle = DateTime.Now.Second * 6f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        initialHours = hours.localRotation;
-        initialMinutes = minutes.localRotation;
-        initialSeconds = seconds.localRotation;
-
-        float hourAngle = (DateTime.Now.Hour % 12) * 30f;
-        float minutesAngle = DateTime.Now.Minute * 6f;
-        float secondsAngle = DateTime.Now.Second * 6f;
-
-        
-        hours.rotation = Quaternion.Euler(hourAngle, 0f, -90f);   
-        minutes.rotation = Quaternion.Euler(minutesAngle, 0f, -90f);
-        seconds.rotation = Quaternion.Euler(secondsAngle, 0f, -90f);
+        UpdateClock();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        UpdateClock();
+
     }
+
+    void UpdateClock() {
+
+        hourAngle = (DateTime.Now.Hour % 12) * 30f + DateTime.Now.Minute * 0.5f;
+        minutesAngle = DateTime.Now.Minute * 6f;
+        secondsAngle = DateTime.Now.Second * 6f;
+
+        hours.rotation = Quaternion.Euler(hourAngle + 90f, 0f, -90f);
+        minutes.rotation = Quaternion.Euler(minutesAngle + 90f, 0f, -90f);
+        seconds.rotation = Quaternion.Euler(secondsAngle + 90f, 0f, -90f);
+
+    }
+
 }
